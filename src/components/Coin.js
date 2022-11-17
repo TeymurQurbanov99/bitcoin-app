e9cfb161cc64c18997abd38d1995c30bcd2964d7
TeymurQurbanov99 <teyqur@gmail.com>
2022-11-17 20:43:35 +0400

    added search bar

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -3,10 +3,10 @@ import React from 'react'
 function Coin({name, icon, price, symbol}) {
   return (
     <div className="coin">
-      <h1>Name: {name}</h1>
+      <h1>{name}</h1>
         <img src={icon}/>
-      <h3>Price: {price}</h3>
-      <h3>Symbol: {symbol}</h3>
+      <h3>Price: {price} $</h3>
+      <h3> {symbol}</h3>
     </div>
   )
 }
