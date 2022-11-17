3cdbc45aa6e3ee159f89effedbfc311696b2c559
TeymurQurbanov99 <teyqur@gmail.com>
2022-11-17 20:08:36 +0400

    managed to display cryptocoins' names

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import './App.css';
+import {useEffect, useState} from 'react'
+import axios from 'axios';
 
 function App() {
+  const [listOfCoins, setListOfCoins] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10").then(
+      (response) => {
+          setListOfCoins(response.data.coins);
+      }
+    )
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cryptoHeader"></div>
+      <div className="cryptoDisplay">
+        {listOfCoins.map((coin) => {
+          return <h1>{coin.name}</h1>
+        })}
+      </div>
     </div>
   );
 }
